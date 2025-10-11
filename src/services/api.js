0f35068ec91a128cb1ef336a749cc57d045f22e5
Vitthal Humbe <vitthal.3454@gmail.com>
2025-10-12 02:11:09 +0530

    Deployment started

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// src/services/api.js
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create a new Axios instance
 const api = axios.create({
